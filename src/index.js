ba93ecc531ec535427c1b22ad8f3b3bf581b86af
Emmanuel Little <109988822+MannyLittle133@users.noreply.github.com>
2023-02-01 14:34:25 -0500

    frontend added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux"
+import { BrowserRouter } from "react-router-dom"
 import './index.css';
 import App from './App';
+import configureStore from "./store"
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +18,14 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  )
+}
+
